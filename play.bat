1f2ed02455416c947eeddb53e5d00668c8c8ad90
Thomas <thomasxin@gmail.com>
2024-04-26 15:01:43 +1000

    update play.bat to support opening file directly

--- play.bat
+++ play.bat
@@ -1,1 +1,7 @@
-py ecdc_stream.py -d %1 | ffplay -f s16le -ac 2 -ar 48k -i -
+if "%~1"=="" (
+	for /f "delims=" %%I in ('powershell -Command "Add-Type -AssemblyName System.Windows.Forms; $dialog = New-Object System.Windows.Forms.OpenFileDialog; $dialog.InitialDirectory = 'C:\\'; $result = $dialog.ShowDialog(); if ($result -eq [System.Windows.Forms.DialogResult]::OK) { $dialog.FileName }"') do (
+		py ecdc_stream.py -d "%%I" | ffplay -f s16le -ac 2 -ar 48k -i -
+	)
+) else (
+	py ecdc_stream.py %2 %3 %4 %5 -d %1 | ffplay -f s16le -ac 2 -ar 48k -i -
+)
